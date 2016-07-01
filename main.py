e43c51770315961965382afa935ae2a9c3d9e412
Kamil Gawlik <kamilmgawlik@gmail.com>
2016-07-01 13:27:08 +0200

    rd,dr update

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print ('hello world')
-print ('hello world')
+pi = 3.1415926536
+def rtod(number):
+    degrees = number*180/pi
+    print(number, "radians are equal to ", int(degrees), " degrees")
+def dtor(number):
+    radians = number * pi / 180
+    print(number, "degrees are equal to ", radians, " radians")
+string = str(input("Podaj liczbe"))
+number = float(string[:-2])
+convertfrom = str(string[-2])
+convertto = str(string[-1])
+
+
+
+if convertfrom == "r":
+    if convertto == "d":
+        rtod(number)
+if convertfrom == "d":
+    if convertto == "r":
+        dtor(number)
